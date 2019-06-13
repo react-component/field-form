04569ca5b6f5b8dda8d029fcff18fe0c0bb14d0b
zombiej <smith3816@gmail.com>
2019-06-13 23:16:48 +0800

    fix prettier

--- tests/legacy/clean-field.test.js
+++ tests/legacy/clean-field.test.js
@@ -7,45 +7,45 @@ import timeout from '../common/timeout';
 
 describe('legacy.clean-field', () => {
   // https://github.com/ant-design/ant-design/issues/12560
-  it('clean field if did update removed', () => {
-      let form;
+  it('clean field if did update removed', async () => {
+    let form;
 
-      const Test = ({ show }) => (
-        <Form
-          ref={instance => {
-            form = instance;
-          }}
-        >
-          {show ? (
-            <Field name="age" rules={[{ required: true }]}>
-              <Input />
-            </Field>
-          ) : (
-            <Field name="name" rules={[{ required: true }]}>
-              <Input />
-            </Field>
-          )}
-        </Form>
-      );
+    const Test = ({ show }) => (
+      <Form
+        ref={instance => {
+          form = instance;
+        }}
+      >
+        {show ? (
+          <Field name="age" rules={[{ required: true }]}>
+            <Input />
+          </Field>
+        ) : (
+          <Field name="name" rules={[{ required: true }]}>
+            <Input />
+          </Field>
+        )}
+      </Form>
+    );
 
-      const wrapper = mount(<Test show />);
+    const wrapper = mount(<Test show />);
 
-      try {
-        await form.validateFields();
-        throw new Error('should not pass');
-      } catch ({ errorFields }) {
-        expect(errorFields.length).toBe(1);
-        expect(errorFields[0].name).toEqual(['age']);
-      }
+    try {
+      await form.validateFields();
+      throw new Error('should not pass');
+    } catch ({ errorFields }) {
+      expect(errorFields.length).toBe(1);
+      expect(errorFields[0].name).toEqual(['age']);
+    }
 
-      wrapper.setProps({ show: false });
+    wrapper.setProps({ show: false });
 
-      try {
-        await form.validateFields();
-        throw new Error('should not pass');
-      } catch ({ errorFields }) {
-        expect(errorFields.length).toBe(1);
-        expect(errorFields[0].name).toEqual(['name']);
-      }
-    });
+    try {
+      await form.validateFields();
+      throw new Error('should not pass');
+    } catch ({ errorFields }) {
+      expect(errorFields.length).toBe(1);
+      expect(errorFields[0].name).toEqual(['name']);
+    }
+  });
 });
