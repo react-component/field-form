0b7be662d5d2c79aee6d056ee8072013bf77f917
zombiej <smith3816@gmail.com>
2019-06-18 18:22:15 +0800

    update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   ...base,
   rules: {
     ...base.rules,
+    'no-template-curly-in-string': 0,
     'prefer-promise-reject-errors': 0,
     'react/no-array-index-key': 0,
   },
