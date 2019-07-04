3601dc640aec604cd1cdee244dd8f5c0890d51f2
David <sumenghua@live.com>
2019-07-04 17:15:15 +0800

    add test for array move function

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,7 +1,15 @@
+import { arrayMove } from '../src/utils/arrayMove';
 import { isSimilar, setValues } from '../src/utils/valueUtil';
 import NameMap from '../src/utils/NameMap';
 
 describe('utils', () => {
+  describe('arrayMove', () => {
+    it('move', () => {
+      expect(arrayMove([0, 1, 2, 3], 0, 2)).toEqual([1, 2, 0, 3]);
+      expect(arrayMove([0, 1, 2, 3], 3, 1)).toEqual([0, 3, 1, 2]);
+      expect(arrayMove([0, 1, 2, 3], 1, 1)).toEqual([0, 1, 2, 3]);
+    });
+  });
   describe('valueUtil', () => {
     it('isSimilar', () => {
       expect(isSimilar(1, 1)).toBeTruthy();
