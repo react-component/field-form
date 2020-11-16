80585738005f6976adbf5b090f22f65f2fda4c31
crazyair <645381995@qq.com>
2020-11-16 18:01:11 +0800

    fix: 404

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   logo:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: '.doc',
+  exportStatic: {},
   // mode: 'site',
   // more config: https://d.umijs.org/config
 });
