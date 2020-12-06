b0049c4afb4f10a76df0e83d286271f9da96bb8b
xrkffgg <xrkffgg@gmail.com>
2020-12-06 20:14:40 +0800

    chore: fix npm lost readme (#237)
    * test
    
    * test
    
    * add script

--- .umirc.ts
+++ .umirc.ts
@@ -8,5 +8,18 @@ export default defineConfig({
   logo:
     'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   outputPath: '.doc',
-  exportStatic: {}
+  exportStatic: {},
+  scripts: [{
+    content: `
+      (function () {
+        var timer = setInterval(function() {
+          try {
+            var menuList = document.getElementsByClassName('__dumi-default-menu-list');
+            menuList[0].childNodes[0].childNodes[0].innerText = 'rc-field-form';
+            clearInterval(timer);
+          } catch (e) {}
+        }, 200)
+      })();
+    `
+  }]
 });
