dfdc3930d9b378a8f291fcc7bf75e1e22a174345
zombiej <smith3816@gmail.com>
2021-01-18 14:51:46 +0800

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,8 @@ module.exports = {
     'jsx-a11y/label-has-associated-control': 0,
     'import/no-extraneous-dependencies': 0,
     'import/no-unresolved': 0,
+    '@typescript-eslint/no-redeclare': 0,
+    '@typescript-eslint/method-signature-style': 0,
+    'no-async-promise-executor': 0,
   },
 };
