9050721cacccc9e670529e27b79b9ba681ab879c
wokeyi <444172954@qq.com>
2021-02-28 21:11:49 +0800

    fix: setFields not working on renderProps

--- src/useForm.ts
+++ src/useForm.ts
@@ -584,7 +584,8 @@ export class FormStore {
     namePathList: InternalNamePath[] | null,
     info: NotifyInfo,
   ) => {
-    if (this.subscribable) {
+    const shloudUpdateField = info.type === 'reset' || info.type === 'setField';
+    if (this.subscribable || shloudUpdateField) {
       const mergedInfo: ValuedNotifyInfo = {
         ...info,
         store: this.getFieldsValue(true),
@@ -592,7 +593,8 @@ export class FormStore {
       this.getFieldEntities().forEach(({ onStoreChange }) => {
         onStoreChange(prevStore, namePathList, mergedInfo);
       });
-    } else {
+    }
+    if (!this.subscribable) {
       this.forceRootUpdate();
     }
   };
