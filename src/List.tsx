90b829823aed7db4716f81228f0c4d9e9bd70e98
二货机器人 <smith3816@gmail.com>
2021-10-29 14:56:42 +0800

    chore: Internal provide ListContext for auto access (#336)
    * refactor: Cache List
    
    * test: test case

--- src/List.tsx
+++ src/List.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import warning from 'rc-util/lib/warning';
-import { InternalNamePath, NamePath, StoreValue, ValidatorRule, Meta } from './interface';
+import type { InternalNamePath, NamePath, StoreValue, ValidatorRule, Meta } from './interface';
 import FieldContext from './FieldContext';
 import Field from './Field';
 import { move, getNamePath } from './utils/valueUtil';
+import type { ListContextProps } from './ListContext';
+import ListContext from './ListContext';
 
 export interface ListField {
   name: number;
@@ -43,14 +45,30 @@ const List: React.FunctionComponent<ListProps> = ({
   });
   const keyManager = keyRef.current;
 
+  const prefixName: InternalNamePath = React.useMemo(() => {
+    const parentPrefixName = getNamePath(context.prefixName) || [];
+    return [...parentPrefixName, ...getNamePath(name)];
+  }, [context.prefixName, name]);
+
+  const fieldContext = React.useMemo(() => ({ ...context, prefixName }), [context, prefixName]);
+
+  // List context
+  const listContext = React.useMemo<ListContextProps>(
+    () => ({
+      getKey: (namePath: InternalNamePath) => {
+        const len = prefixName.length;
+        const pathName = namePath[len];
+        return keyManager.keys[pathName];
+      },
+    }),
+    [prefixName],
+  );
+
   // User should not pass `children` as other type.
   if (typeof children !== 'function') {
     warning(false, 'Form.List only accepts function as children.');
     return null;
   }
-
-  const parentPrefixName = getNamePath(context.prefixName) || [];
-  const prefixName: InternalNamePath = [...parentPrefixName, ...getNamePath(name)];
 
   const shouldUpdate = (prevValue: StoreValue, nextValue: StoreValue, { source }) => {
     if (source === 'internal') {
@@ -60,93 +78,98 @@ const List: React.FunctionComponent<ListProps> = ({
   };
 
   return (
-    <FieldContext.Provider value={{ ...context, prefixName }}>
-      <Field
-        name={[]}
-        shouldUpdate={shouldUpdate}
-        rules={rules}
-        validateTrigger={validateTrigger}
-        initialValue={initialValue}
-        isList
-      >
-        {({ value = [], onChange }, meta) => {
-          const { getFieldValue } = context;
-          const getNewValue = () => {
-            const values = getFieldValue(prefixName || []) as StoreValue[];
-            return values || [];
-          };
-          /**
-           * Always get latest value in case user update fields by `form` api.
-           */
-          const operations: ListOperations = {
-            add: (defaultValue, index?: number) => {
-              // Mapping keys
-              const newValue = getNewValue();
+    <ListContext.Provider value={listContext}>
+      <FieldContext.Provider value={fieldContext}>
+        <Field
+          name={[]}
+          shouldUpdate={shouldUpdate}
+          rules={rules}
+          validateTrigger={validateTrigger}
+          initialValue={initialValue}
+          isList
+        >
+          {({ value = [], onChange }, meta) => {
+            const { getFieldValue } = context;
+            const getNewValue = () => {
+              const values = getFieldValue(prefixName || []) as StoreValue[];
+              return values || [];
+            };
+            /**
+             * Always get latest value in case user update fields by `form` api.
+             */
+            const operations: ListOperations = {
+              add: (defaultValue, index?: number) => {
+                // Mapping keys
+                const newValue = getNewValue();
 
-              if (index >= 0 && index <= newValue.length) {
-                keyManager.keys = [
-                  ...keyManager.keys.slice(0, index),
-                  keyManager.id,
-                  ...keyManager.keys.slice(index),
-                ];
-                onChange([...newValue.slice(0, index), defaultValue, ...newValue.slice(index)]);
-              } else {
-                if (
-                  process.env.NODE_ENV !== 'production' &&
-                  (index < 0 || index > newValue.length)
-                ) {
-                  warning(
-                    false,
-                    'The second parameter of the add function should be a valid positive number.',
-                  );
+                if (index >= 0 && index <= newValue.length) {
+                  keyManager.keys = [
+                    ...keyManager.keys.slice(0, index),
+                    keyManager.id,
+                    ...keyManager.keys.slice(index),
+                  ];
+                  onChange([...newValue.slice(0, index), defaultValue, ...newValue.slice(index)]);
+                } else {
+                  if (
+                    process.env.NODE_ENV !== 'production' &&
+                    (index < 0 || index > newValue.length)
+                  ) {
+                    warning(
+                      false,
+                      'The second parameter of the add function should be a valid positive number.',
+                    );
+                  }
+                  keyManager.keys = [...keyManager.keys, keyManager.id];
+                  onChange([...newValue, defaultValue]);
                 }
-                keyManager.keys = [...keyManager.keys, keyManager.id];
-                onChange([...newValue, defaultValue]);
+                keyManager.id += 1;
+              },
+              remove: (index: number | number[]) => {
+                const newValue = getNewValue();
+                const indexSet = new Set(Array.isArray(index) ? index : [index]);
+
+                if (indexSet.size <= 0) {
+                  return;
+                }
+                keyManager.keys = keyManager.keys.filter(
+                  (_, keysIndex) => !indexSet.has(keysIndex),
+                );
+
+                // Trigger store change
+                onChange(newValue.filter((_, valueIndex) => !indexSet.has(valueIndex)));
+              },
+              move(from: number, to: number) {
+                if (from === to) {
+                  return;
+                }
+                const newValue = getNewValue();
+
+                // Do not handle out of range
+                if (from < 0 || from >= newValue.length || to < 0 || to >= newValue.length) {
+                  return;
+                }
+
+                keyManager.keys = move(keyManager.keys, from, to);
+
+                // Trigger store change
+                onChange(move(newValue, from, to));
+              },
+            };
+
+            let listValue = value || [];
+            if (!Array.isArray(listValue)) {
+              listValue = [];
+
+              if (process.env.NODE_ENV !== 'production') {
+                warning(
+                  false,
+                  `Current value of '${prefixName.join(' > ')}' is not an array type.`,
+                );
               }
-              keyManager.id += 1;
-            },
-            remove: (index: number | number[]) => {
-              const newValue = getNewValue();
-              const indexSet = new Set(Array.isArray(index) ? index : [index]);
-
-              if (indexSet.size <= 0) {
-                return;
-              }
-              keyManager.keys = keyManager.keys.filter((_, keysIndex) => !indexSet.has(keysIndex));
-
-              // Trigger store change
-              onChange(newValue.filter((_, valueIndex) => !indexSet.has(valueIndex)));
-            },
-            move(from: number, to: number) {
-              if (from === to) {
-                return;
-              }
-              const newValue = getNewValue();
-
-              // Do not handle out of range
-              if (from < 0 || from >= newValue.length || to < 0 || to >= newValue.length) {
-                return;
-              }
-
-              keyManager.keys = move(keyManager.keys, from, to);
-
-              // Trigger store change
-              onChange(move(newValue, from, to));
-            },
-          };
-
-          let listValue = value || [];
-          if (!Array.isArray(listValue)) {
-            listValue = [];
-
-            if (process.env.NODE_ENV !== 'production') {
-              warning(false, `Current value of '${prefixName.join(' > ')}' is not an array type.`);
             }
-          }
 
-          return children(
-            (listValue as StoreValue[]).map(
-              (__, index): ListField => {
+            return children(
+              (listValue as StoreValue[]).map((__, index): ListField => {
                 let key = keyManager.keys[index];
                 if (key === undefined) {
                   keyManager.keys[index] = keyManager.id;
@@ -159,14 +182,14 @@ const List: React.FunctionComponent<ListProps> = ({
                   key,
                   isListField: true,
                 };
-              },
-            ),
-            operations,
-            meta,
-          );
-        }}
-      </Field>
-    </FieldContext.Provider>
+              }),
+              operations,
+              meta,
+            );
+          }}
+        </Field>
+      </FieldContext.Provider>
+    </ListContext.Provider>
   );
 };
 
