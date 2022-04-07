d6bd249635171aaa8547c0d524f6dd22292b104b
crazyair <645381995@qq.com>
2022-04-07 21:32:56 +0800

    feat: add test

--- tests/useWatch.test.tsx
+++ tests/useWatch.test.tsx
@@ -101,18 +101,50 @@ describe('useWatch', () => {
 
       return (
         <div>
-          <Form
-            form={form}
-            initialValues={{ name: 'bamboo' }}
-            ref={instance => {
-              staticForm = instance;
-            }}
-          >
+          <Form form={form} initialValues={{ name: 'bamboo' }}>
             {visible && (
               <Field name="name">
                 <Input />
               </Field>
             )}
+          </Form>
+          <div className="values">{JSON.stringify(values)}</div>
+        </div>
+      );
+    };
+    await act(async () => {
+      const wrapper = mount(<Demo visible />);
+      await timeout();
+      wrapper.setProps({ visible: false });
+      expect(wrapper.find('.values').at(0).getDOMNode().innerHTML).toBe(
+        JSON.stringify({ name: undefined }),
+      );
+      wrapper.setProps({ visible: true });
+      await timeout();
+      expect(wrapper.find('.values').at(0).getDOMNode().innerHTML).toBe(
+        JSON.stringify({ name: 'bamboo' }),
+      );
+    });
+  });
+  it('unmount useWatch', async () => {
+    const DemoWatch = ({ form }: { form: FormInstance }) => {
+      Form.useWatch({ form, dependencies: ['name'] });
+
+      return (
+        <Field name="name">
+          <Input />
+        </Field>
+      );
+    };
+
+    const Demo = ({ visible }: { visible: boolean }) => {
+      const [form] = Form.useForm();
+      const values = Form.useWatch({ form, dependencies: ['name'] });
+
+      return (
+        <div>
+          <Form form={form} initialValues={{ name: 'bamboo' }}>
+            {visible && <DemoWatch form={form} />}
           </Form>
           <div className="values">{JSON.stringify(values)}</div>
         </div>
