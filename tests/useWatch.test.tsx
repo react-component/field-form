df922c6ff8538b5fd398297e9c1f24709496cb1d
crazyair <645381995@qq.com>
2022-04-07 17:57:23 +0800

    feat: add test

--- tests/useWatch.test.tsx
+++ tests/useWatch.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import type { FormInstance } from '../src';
 import Form, { Field } from '../src';
 import timeout from './common/timeout';
 
 describe('useWatch', () => {
-  it('base', async () => {
+  let staticForm: FormInstance<any> = null;
+  it('field initialValue', async () => {
     const Demo = () => {
       const [form] = Form.useForm();
       const values = Form.useWatch({ form, dependencies: ['name'] });
@@ -20,8 +22,62 @@ describe('useWatch', () => {
     };
     const wrapper = mount(<Demo />);
     await timeout();
-    expect(wrapper.find('.values').last().getDOMNode().innerHTML).toBe(
+    expect(wrapper.find('.values').at(0).getDOMNode().innerHTML).toBe(
       JSON.stringify({ name: 'bamboo' }),
+    );
+  });
+  it('form initialValue', async () => {
+    const Demo = () => {
+      const [form] = Form.useForm();
+      const values = Form.useWatch({ form, dependencies: ['name'] });
+
+      return (
+        <div>
+          <Form form={form} initialValues={{ name: 'bamboo', other: 'other' }}>
+            <Field name="name" />
+          </Form>
+          <div className="values">{JSON.stringify(values)}</div>
+        </div>
+      );
+    };
+    const wrapper = mount(<Demo />);
+    await timeout();
+    expect(wrapper.find('.values').at(0).getDOMNode().innerHTML).toBe(
+      JSON.stringify({ name: 'bamboo' }),
+    );
+  });
+  it('change name', async () => {
+    const Demo = () => {
+      const [form] = Form.useForm();
+      const values = Form.useWatch({ form, dependencies: ['name'] });
+
+      return (
+        <div>
+          <Form
+            form={form}
+            ref={instance => {
+              staticForm = instance;
+            }}
+          >
+            <Field name="name" />
+          </Form>
+          <div className="values">{JSON.stringify(values)}</div>
+        </div>
+      );
+    };
+    const wrapper = mount(<Demo />);
+    await timeout();
+    staticForm.setFields([{ name: 'name', value: 'setFields' }]);
+    expect(wrapper.find('.values').at(0).getDOMNode().innerHTML).toBe(
+      JSON.stringify({ name: 'setFields' }),
+    );
+    staticForm.setFieldsValue({ name: 'setFieldsValue' });
+    expect(wrapper.find('.values').at(0).getDOMNode().innerHTML).toBe(
+      JSON.stringify({ name: 'setFieldsValue' }),
+    );
+    staticForm.resetFields();
+    expect(wrapper.find('.values').at(0).getDOMNode().innerHTML).toBe(
+      JSON.stringify({ name: undefined }),
     );
   });
 });
