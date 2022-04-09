ac4314857298b2d8dc18878b00146fd6afa5e649
crazyair <645381995@qq.com>
2022-04-09 18:11:58 +0800

    feat: watch

--- src/useWatch.ts
+++ src/useWatch.ts
@@ -5,26 +5,26 @@ import type { InternalFormInstance, NamePath } from './interface';
 import { useState, useEffect, useRef, useContext } from 'react';
 import { getNamePath, containsNamePath } from './utils/valueUtil';
 
-const useWatch = <Values>(dependencies?: NamePath[], form?: FormInstance<Values>) => {
+const useWatch = <Values = any>(dependencies?: NamePath[], form?: FormInstance<Values>) => {
   const [values, setValues] = useState<Values>({} as Values);
   const watchIdRef = useRef<symbol>(Symbol('watchId'));
-  const isDrop = useRef(false);
+  const isUnmount = useRef(false);
 
   const fieldContext = useContext(FieldContext);
   const { getFieldsValue, getInternalHooks } = (form || fieldContext) as InternalFormInstance;
   const { setWatchCallbacks } = getInternalHooks(HOOK_MARK);
 
   useEffect(() => {
-    isDrop.current = false;
+    isUnmount.current = false;
     return () => {
-      isDrop.current = true;
+      isUnmount.current = true;
     };
   }, []);
 
   useEffect(() => {
     setWatchCallbacks(watchIdRef.current, {
       onValuesChange: ({ namePathList, registerValues }) => {
-        if (isDrop.current) return;
+        if (isUnmount.current) return;
         const dependencyList = dependencies?.map(getNamePath);
         if (dependencies && namePathList) {
           const nameList = namePathList?.map(getNamePath);
