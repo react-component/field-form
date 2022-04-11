2bf20112e2a144359d9eb70ee961e6ffbf215b0f
crazyair <645381995@qq.com>
2022-04-11 11:45:21 +0800

    feat: review

--- src/useWatch.ts
+++ src/useWatch.ts
@@ -8,23 +8,21 @@ import { getNamePath, containsNamePath } from './utils/valueUtil';
 const useWatch = <Values = any>(dependencies?: NamePath[], form?: FormInstance<Values>) => {
   const [values, setValues] = useState<Values>({} as Values);
   const watchIdRef = useRef<Record<string, any>>({});
-  const isUnmount = useRef(false);
 
   const fieldContext = useContext(FieldContext);
   const { getFieldsValue, getInternalHooks } = (form || fieldContext) as InternalFormInstance;
   const { setWatchCallbacks } = getInternalHooks(HOOK_MARK);
 
   useEffect(() => {
-    isUnmount.current = false;
+    const id = watchIdRef.current;
     return () => {
-      isUnmount.current = true;
+      setWatchCallbacks(id, {});
     };
-  }, []);
+  }, [setWatchCallbacks]);
 
   useMemo(() => {
     setWatchCallbacks(watchIdRef.current, {
       onFieldsChange: (namePathList, registerValues) => {
-        if (isUnmount.current) return;
         const dependencyList = dependencies?.map(getNamePath);
         const nameList = namePathList?.map(getNamePath);
         if (dependencies && namePathList) {
