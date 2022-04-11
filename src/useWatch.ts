f59a635c2563f0589a551491097b9523e9109085
crazyair <645381995@qq.com>
2022-04-11 12:09:42 +0800

    feat: review

--- src/useWatch.ts
+++ src/useWatch.ts
@@ -2,7 +2,7 @@ import type { FormInstance } from '.';
 import { FieldContext } from '.';
 import { HOOK_MARK } from './FieldContext';
 import type { InternalFormInstance, NamePath } from './interface';
-import { useState, useRef, useContext, useMemo, useEffect } from 'react';
+import { useState, useRef, useContext, useEffect } from 'react';
 import { getNamePath, containsNamePath } from './utils/valueUtil';
 
 const useWatch = <Values = any>(dependencies?: NamePath[], form?: FormInstance<Values>) => {
@@ -20,21 +20,19 @@ const useWatch = <Values = any>(dependencies?: NamePath[], form?: FormInstance<V
     };
   }, [setWatchCallbacks]);
 
-  useMemo(() => {
-    setWatchCallbacks(watchIdRef.current, {
-      onFieldsChange: (namePathList, registerValues) => {
-        const dependencyList = dependencies?.map(getNamePath);
-        const nameList = namePathList?.map(getNamePath);
-        if (dependencies && namePathList) {
-          if (dependencyList.some(dependency => containsNamePath(nameList, dependency))) {
-            setValues(getFieldsValue());
-          }
-        } else {
-          setValues(registerValues || getFieldsValue());
+  setWatchCallbacks(watchIdRef.current, {
+    onFieldsChange: (namePathList, registerValues) => {
+      const dependencyList = dependencies?.map(getNamePath);
+      const nameList = namePathList?.map(getNamePath);
+      if (dependencies && namePathList) {
+        if (dependencyList.some(dependency => containsNamePath(nameList, dependency))) {
+          setValues(getFieldsValue());
         }
-      },
-    });
-  }, [dependencies, getFieldsValue, setWatchCallbacks]);
+      } else {
+        setValues(registerValues || getFieldsValue());
+      }
+    },
+  });
 
   return values;
 };
