270f8826de26b838a8bd16a57fe8667578d54b0f
crazyair <645381995@qq.com>
2022-04-14 15:10:20 +0800

    feat: add test

--- src/useWatch.ts
+++ src/useWatch.ts
@@ -6,40 +6,42 @@ import type { InternalFormInstance, NamePath, Store } from './interface';
 import { useState, useContext, useEffect, useRef } from 'react';
 import { getNamePath, getValue } from './utils/valueUtil';
 
+type GetGeneric<Type> = Type extends FormInstance<infer ValueType> ? ValueType : never;
+
 function useWatch<
-  TDependencies1 extends keyof TForm['values'],
+  TDependencies1 extends keyof GetGeneric<TForm>,
   TForm extends FormInstance,
-  TDependencies2 extends keyof TForm['values'][TDependencies1],
-  TDependencies3 extends keyof TForm['values'][TDependencies1][TDependencies2],
-  TDependencies4 extends keyof TForm['values'][TDependencies1][TDependencies2][TDependencies3],
+  TDependencies2 extends keyof GetGeneric<TForm>[TDependencies1],
+  TDependencies3 extends keyof GetGeneric<TForm>[TDependencies1][TDependencies2],
+  TDependencies4 extends keyof GetGeneric<TForm>[TDependencies1][TDependencies2][TDependencies3],
 >(
   dependencies: [TDependencies1, TDependencies2, TDependencies3, TDependencies4],
   form?: TForm,
-): TForm['values'][TDependencies1][TDependencies2][TDependencies3][TDependencies4];
+): GetGeneric<TForm>[TDependencies1][TDependencies2][TDependencies3][TDependencies4];
 
 function useWatch<
-  TDependencies1 extends keyof TForm['values'],
+  TDependencies1 extends keyof GetGeneric<TForm>,
   TForm extends FormInstance,
-  TDependencies2 extends keyof TForm['values'][TDependencies1],
-  TDependencies3 extends keyof TForm['values'][TDependencies1][TDependencies2],
+  TDependencies2 extends keyof GetGeneric<TForm>[TDependencies1],
+  TDependencies3 extends keyof GetGeneric<TForm>[TDependencies1][TDependencies2],
 >(
   dependencies: [TDependencies1, TDependencies2, TDependencies3],
   form?: TForm,
-): TForm['values'][TDependencies1][TDependencies2][TDependencies3];
+): GetGeneric<TForm>[TDependencies1][TDependencies2][TDependencies3];
 
 function useWatch<
-  TDependencies1 extends keyof TForm['values'],
+  TDependencies1 extends keyof GetGeneric<TForm>,
   TForm extends FormInstance,
-  TDependencies2 extends keyof TForm['values'][TDependencies1],
+  TDependencies2 extends keyof GetGeneric<TForm>[TDependencies1],
 >(
   dependencies: [TDependencies1, TDependencies2],
   form?: TForm,
-): TForm['values'][TDependencies1][TDependencies2];
+): GetGeneric<TForm>[TDependencies1][TDependencies2];
 
-function useWatch<TDependencies extends keyof TForm['values'], TForm extends FormInstance>(
+function useWatch<TDependencies extends keyof GetGeneric<TForm>, TForm extends FormInstance>(
   dependencies: TDependencies | [TDependencies],
   form?: TForm,
-): TForm['values'][TDependencies];
+): GetGeneric<TForm>[TDependencies];
 
 function useWatch<TForm extends FormInstance>(dependencies: NamePath, form?: TForm): any;
 
