46d92dbaa35f22124efba48f5bae1943b5c7e9da
Godfery Yang <hiyangguo@qq.com>
2022-05-25 21:48:01 +0800

    Added missing props

--- src/interface.ts
+++ src/interface.ts
@@ -53,6 +53,8 @@ export interface ValidatorRule {
   warningOnly?: boolean;
   message?: string | ReactElement;
   validator: Validator;
+  /** Customize rule level `validateTrigger`. Must be subset of Field `validateTrigger` */
+  validateTrigger?: string | string[];
 }
 
 interface BaseRule {
