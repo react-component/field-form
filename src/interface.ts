5485e45eed435c050730a40e213fe3ca12a11469
crazyair <645381995@qq.com>
2023-07-14 11:09:04 +0800

    feat: test

--- src/interface.ts
+++ src/interface.ts
@@ -1,9 +1,9 @@
 import type { ReactElement } from 'react';
-import type { DefineNamePath } from './namePathType';
+import type { DeepNamePath } from './namePathType';
 import type { ReducerAction } from './useForm';
 
 export type InternalNamePath = (string | number)[];
-export type NamePath<T = any> = DefineNamePath<T>;
+export type NamePath<T = any> = DeepNamePath<T>;
 
 export type StoreValue = any;
 export type Store = Record<string, StoreValue>;
--- src/namePathType.ts
+++ src/namePathType.ts
@@ -1,8 +1,8 @@
-export type DefineNamePath<T, T1 extends any[] = []> = {
+export type DeepNamePath<T, T1 extends any[] = []> = {
   [P in keyof Required<T>]:
     | (T1[0] extends undefined ? P : [...T1, P])
     | [...T1, P]
     | (Required<T>[P] extends Record<string, any>
-        ? DefineNamePath<Required<T>[P], [...T1, P]>
+        ? DeepNamePath<Required<T>[P], [...T1, P]>
         : never);
 }[keyof T];
