08da61c0f5c23372a019589ff7d0156b778e5ef1
crazyair <645381995@qq.com>
2023-07-14 09:53:08 +0800

    feat: test

--- src/namePathType.ts
+++ src/namePathType.ts
@@ -1,14 +1,8 @@
-export type DefineNamePath<T, T1 extends any[] = []> = T extends (string | number)[]
-  ? [...T1, number]
-  : T extends Record<string, any>[]
-  ? [...T1, number] | DefineNamePath<Required<T>[number], [...T1, number]>
-  : T extends Record<string, any>
-  ? {
-      [P in keyof T]:
-        | (T1[0] extends undefined ? P : [...T1, P])
-        | [...T1, P]
-        | (Required<T>[P] extends Record<string, any>
-            ? [...T1, P] | DefineNamePath<Required<T>[P], [...T1, P]>
-            : never);
-    }[keyof T]
-  : never;
+export type DefineNamePath<T, T1 extends any[] = []> = {
+  [P in keyof Required<T>]:
+    | (T1[0] extends undefined ? P : [...T1, P])
+    | [...T1, P]
+    | (Required<T>[P] extends Record<string, any>
+        ? DefineNamePath<Required<T>[P], [...T1, P]>
+        : never);
+}[keyof T];
