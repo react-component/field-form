b73d74b8aafca6ce17d07c4ea28709e816c32da5
crazyair <645381995@qq.com>
2023-07-15 22:16:53 +0800

    feat: test

--- src/namePathType.ts
+++ src/namePathType.ts
@@ -1,22 +1,19 @@
-export type DeepNamePath2<T = unknown, T1 extends any[] = []> = any extends T
-  ? never
-  : Required<T> extends []
-  ? Required<T>
-  : {
-      [P in keyof Required<T>]:
-        | (T1['length'] extends 0 ? P | [...T1, P] : never)
-        | (Required<T>[P] extends (string | number)[]
-            ? [...T1, P] | [...T1, P, number]
-            : Required<T>[P] extends any[]
-            ?
-                | [...T1, P]
-                | [...T1, P, number]
-                | DeepNamePath2<Required<T>[P][number], [...T1, P, number]>
-            : Required<T>[P] extends Record<string, unknown>
-            ? [...T1, P] | DeepNamePath2<Required<T>[P], [...T1, P]>
-            : [...T1, P]);
-    }[keyof T];
-
-export type DeepNamePath<T = any> =
+export type DeepNamePath<T = unknown, T1 extends any[] = []> =
   | (T extends string | number | (string | number)[] ? T : never)
-  | DeepNamePath2<T>;
+  | (any extends T
+      ? never
+      : Required<T> extends []
+      ? Required<T>
+      : {
+          [P in keyof Required<T>]:
+            | (T1['length'] extends 0 ? P | [...T1, P] : never)
+            | (Required<T>[P] extends any[]
+                ?
+                    | ([...T1, P] | [...T1, P, number])
+                    | (Required<T>[P] extends (string | number)[]
+                        ? never
+                        : DeepNamePath<Required<T>[P][number], [...T1, P, number]>)
+                : Required<T>[P] extends Record<string, any>
+                ? [...T1, P] | DeepNamePath<Required<T>[P], [...T1, P]>
+                : [...T1, P]);
+        }[keyof T]);
