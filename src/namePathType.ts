8c9c11bb45f669d0b62fe5424bf25e84e98811fb
crazyair <645381995@qq.com>
2023-07-29 00:56:41 +0800

    feat: 重新整理类型

--- src/namePathType.ts
+++ src/namePathType.ts
@@ -2,27 +2,49 @@
  * Store: The store type from `FormInstance<Store>`
  * ParentNamePath: Auto generate by nest logic. Do not fill manually.
  */
-export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> =
-  // Limit 10
-  ParentNamePath['length'] extends 10
-    ? never
-    : // Return if Store is basic type
-    Store extends string | number | (string | number)[]
-    ? Store
-    : Store extends Record<string, any>[]
-    ? [...ParentNamePath, number] | DeepNamePath<Store[number], [...ParentNamePath, number]>
-    : Store extends Record<string, any>
-    ? {
-        // If Store is object.
-        // Let's create an <Store.Key, RootStore.KeyPath> map
-        // And get from the map by Store.Key.
-        [FieldKey in keyof Store]:
-          | (ParentNamePath['length'] extends 0 ? FieldKey : never)
-          | [...ParentNamePath, FieldKey]
-          | (Store[FieldKey] extends (number | string)[]
-              ? [...ParentNamePath, FieldKey, number]
-              : Store[FieldKey] extends Record<string, any>
-              ? DeepNamePath<Store[FieldKey], [...ParentNamePath, FieldKey]>
-              : never);
-      }[keyof Store]
-    : never;
+// export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> =
+//   // Limit 10
+//   ParentNamePath['length'] extends 10
+//     ? never
+//     : // Return if Store is basic type
+//     Store extends string | number | (string | number)[]
+//     ? Store
+//     : Store extends Record<string, any>[]
+//     ? [...ParentNamePath, number] | DeepNamePath<Store[number], [...ParentNamePath, number]>
+//     : Store extends Record<string, any>
+//     ? {
+//         // If Store is object.
+//         // Let's create an <Store.Key, RootStore.KeyPath> map
+//         // And get from the map by Store.Key.
+//         [FieldKey in keyof Store]:
+//           | (ParentNamePath['length'] extends 0 ? FieldKey : never)
+//           | [...ParentNamePath, FieldKey]
+//           | (Store[FieldKey] extends (number | string)[]
+//               ? [...ParentNamePath, FieldKey, number]
+//               : Store[FieldKey] extends Record<string, any>
+//               ? DeepNamePath<Store[FieldKey], [...ParentNamePath, FieldKey]>
+//               : never);
+//       }[keyof Store]
+//     : never;
+
+export type DeepObject<Store = any, ParentNamePath extends any[] = []> = {
+  [FieldKey in keyof Store]:
+    | (ParentNamePath['length'] extends 0 ? FieldKey : never)
+    | [...ParentNamePath, FieldKey]
+    | (Store[FieldKey] extends (number | string)[]
+        ? [...ParentNamePath, FieldKey, number]
+        : Store[FieldKey] extends Record<string, any>
+        ? DeepNamePath<Store[FieldKey], [...ParentNamePath, FieldKey]>
+        : never);
+}[keyof Store];
+
+export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> = Store extends
+  | string
+  | number
+  | (string | number)[]
+  ? Store
+  : Store extends Record<string, any>[]
+  ? [...ParentNamePath, number] | DeepObject<Store[number], [...ParentNamePath, number]>
+  : Store extends Record<string, any>
+  ? DeepObject<Store, ParentNamePath>
+  : never;
