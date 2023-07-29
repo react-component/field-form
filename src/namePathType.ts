aac397f3fe420930472d828c7afaf66b491d0b90
二货机器人 <smith3816@gmail.com>
2023-07-29 20:52:36 +0800

    chore: update ts

--- src/namePathType.ts
+++ src/namePathType.ts
@@ -2,57 +2,34 @@
  * Store: The store type from `FormInstance<Store>`
  * ParentNamePath: Auto generate by nest logic. Do not fill manually.
  */
-// export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> = any extends Store
-//   ? // Return if Store is basic type
-//     Store extends string | number | (string | number)[]
-//     ? Store
-//     : never
-//   : // If Store is object.
-//     // Let's create an <Store.Key, RootStore.KeyPath> map
-//     // And get from the map by Store.Key.
-//     {
-//       [FieldKey in keyof Required<Store>]:
-//         | (ParentNamePath['length'] extends 0 ? FieldKey | [...ParentNamePath, FieldKey] : never)
-//         | (Required<Store>[FieldKey] extends any[]
-//             ? // If is a validate array path
-//               | ([...ParentNamePath, FieldKey] | [...ParentNamePath, FieldKey, number])
-//                 | (Required<Store>[FieldKey] extends (string | number)[]
-//                     ? never
-//                     : // Dig with [...ParentNamePath, FieldKey, number]
-//                       DeepNamePath<
-//                         Required<Store>[FieldKey][number],
-//                         [...ParentNamePath, FieldKey, number]
-//                       >)
-//             : Required<Store>[FieldKey] extends Record<string, any>
-//             ? // If value is a object, dig into value.
-//               | [...ParentNamePath, FieldKey]
-//                 | DeepNamePath<Required<Store>[FieldKey], [...ParentNamePath, FieldKey]>
-//             : [...ParentNamePath, FieldKey]);
-//     }[keyof Store];
-
-/**
- * Store: The store type from `FormInstance<Store>`
- * ParentNamePath: Auto generate by nest logic. Do not fill manually.
- */
-export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> = any extends Store
-  ? Store extends string | number
+export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> =
+  // Follow code is batch check if `Store` is base type
+  Store extends string | number
     ? string | number
     : string[] extends Store
     ? Store
     : number[] extends Store
     ? Store
-    : never
-  : Store extends Record<string, any>[]
-  ? [...ParentNamePath, number] | DeepNamePath<Store[number], [...ParentNamePath, number]>
-  : Store extends Record<string, any>
-  ? {
-      [FieldKey in keyof Store]:
-        | (ParentNamePath['length'] extends 0 ? FieldKey : never)
-        | [...ParentNamePath, FieldKey]
-        | (Store[FieldKey] extends (number | string)[]
-            ? [...ParentNamePath, FieldKey, number]
-            : Store[FieldKey] extends Record<string, any>
-            ? DeepNamePath<Store[FieldKey], [...ParentNamePath, FieldKey]>
-            : never);
-    }[keyof Store]
-  : never;
+    : // Check if `Store` is `object[]`
+    Store extends Record<string, any>[]
+    ? // Connect path. e.g. { a: { b: string }[] }
+      // Get: [a] | [ a,number] | [ a ,number , b]
+      [...ParentNamePath, number] | DeepNamePath<Store[number], [...ParentNamePath, number]>
+    : // Check if `Store` is `object`
+    Store extends Record<string, any>
+    ? // Convert `Store` to <key, value>. We mark key a `FieldKey`
+      {
+        [FieldKey in keyof Store]:
+          | // If `ParentNamePath` is empty, it can use `FieldKey` without array path
+          (ParentNamePath['length'] extends 0 ? FieldKey : never)
+          // Exist `ParentNamePath`, connect it
+          | [...ParentNamePath, FieldKey]
+          | (Store[FieldKey] extends (number | string)[]
+              ? // If `Store[FieldKey]` is base array type
+                [...ParentNamePath, FieldKey, number]
+              : Store[FieldKey] extends Record<string, any>
+              ? // If `Store[FieldKey]` is object
+                DeepNamePath<Store[FieldKey], [...ParentNamePath, FieldKey]>
+              : never);
+      }[keyof Store]
+    : never;
