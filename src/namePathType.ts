316ece44bc5ba4ac136a9712d7056a80fb28a2ed
crazyair <645381995@qq.com>
2023-07-29 21:01:15 +0800

    feat: test

--- src/namePathType.ts
+++ src/namePathType.ts
@@ -10,26 +10,20 @@ export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> =
     ? Store
     : number[] extends Store
     ? Store
-    : // Check if `Store` is `object[]`
-    Store extends Record<string, any>[]
+    : Store extends Record<string, any>[] // Check if `Store` is `object[]`
     ? // Connect path. e.g. { a: { b: string }[] }
       // Get: [a] | [ a,number] | [ a ,number , b]
       [...ParentNamePath, number] | DeepNamePath<Store[number], [...ParentNamePath, number]>
-    : // Check if `Store` is `object`
-    Store extends Record<string, any>
-    ? // Convert `Store` to <key, value>. We mark key a `FieldKey`
-      {
+    : Store extends Record<string, any> // Check if `Store` is `object`
+    ? {
+        // Convert `Store` to <key, value>. We mark key a `FieldKey`
         [FieldKey in keyof Store]:
-          | // If `ParentNamePath` is empty, it can use `FieldKey` without array path
-          (ParentNamePath['length'] extends 0 ? FieldKey : never)
-          // Exist `ParentNamePath`, connect it
-          | [...ParentNamePath, FieldKey]
+          | (ParentNamePath['length'] extends 0 ? FieldKey : never) // If `ParentNamePath` is empty, it can use `FieldKey` without array path
+          | [...ParentNamePath, FieldKey] // Exist `ParentNamePath`, connect it
           | (Store[FieldKey] extends (number | string)[]
-              ? // If `Store[FieldKey]` is base array type
-                [...ParentNamePath, FieldKey, number]
+              ? [...ParentNamePath, FieldKey, number] // If `Store[FieldKey]` is base array type
               : Store[FieldKey] extends Record<string, any>
-              ? // If `Store[FieldKey]` is object
-                DeepNamePath<Store[FieldKey], [...ParentNamePath, FieldKey]>
+              ? DeepNamePath<Store[FieldKey], [...ParentNamePath, FieldKey]> // If `Store[FieldKey]` is object
               : never);
       }[keyof Store]
     : never;
