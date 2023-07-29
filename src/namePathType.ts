a6d8cf989ddbfee3b33e666fe44a1967023d1bc5
二货机器人 <smith3816@gmail.com>
2023-07-29 21:50:59 +0800

    chore: update ts

--- src/namePathType.ts
+++ src/namePathType.ts
@@ -1,3 +1,11 @@
+import type { Moment } from 'moment';
+import type { Dayjs } from 'dayjs';
+
+/**
+ * Remove type from Object
+ */
+type PureObject<T> = Omit<T, keyof Object>;
+
 /**
  * Store: The store type from `FormInstance<Store>`
  * ParentNamePath: Auto generate by nest logic. Do not fill manually.
@@ -13,7 +21,8 @@ export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> =
     : Store extends Record<string, any>[] // Check if `Store` is `object[]`
     ? // Connect path. e.g. { a: { b: string }[] }
       // Get: [a] | [ a,number] | [ a ,number , b]
-      [...ParentNamePath, number] | DeepNamePath<Store[number], [...ParentNamePath, number]>
+      | [...ParentNamePath, number]
+        | DeepNamePath<PureObject<Store[number]>, [...ParentNamePath, number]>
     : Store extends Record<string, any> // Check if `Store` is `object`
     ? {
         // Convert `Store` to <key, value>. We mark key a `FieldKey`
@@ -23,7 +32,13 @@ export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> =
           | (Store[FieldKey] extends (number | string)[]
               ? [...ParentNamePath, FieldKey, number] // If `Store[FieldKey]` is base array type
               : Store[FieldKey] extends Record<string, any>
-              ? DeepNamePath<Store[FieldKey], [...ParentNamePath, FieldKey]> // If `Store[FieldKey]` is object
+              ? DeepNamePath<PureObject<Store[FieldKey]>, [...ParentNamePath, FieldKey]> // If `Store[FieldKey]` is object
               : never);
       }[keyof Store]
     : never;
+
+type Test = {
+  date: Moment;
+};
+
+type CCC = DeepNamePath<Test>;
