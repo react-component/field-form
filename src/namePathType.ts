173dd7a3e3701f1faee00a65640600ecba693598
crazyair <645381995@qq.com>
2023-07-29 22:10:52 +0800

    feat: test

--- src/namePathType.ts
+++ src/namePathType.ts
@@ -1,19 +1,11 @@
-import type { Moment } from 'moment';
-import type { Dayjs } from 'dayjs';
-
-/**
- * Remove type from Object
- */
-type PureObject<T> = Omit<T, keyof Object>;
-
 /**
  * Store: The store type from `FormInstance<Store>`
  * ParentNamePath: Auto generate by nest logic. Do not fill manually.
  */
-export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> =
+export type DeepNamePathBase<Store = any, ParentNamePath extends any[] = []> =
   // Follow code is batch check if `Store` is base type
   Store extends string | number
-    ? string | number
+    ? Store
     : string[] extends Store
     ? Store
     : number[] extends Store
@@ -21,8 +13,9 @@ export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> =
     : Store extends Record<string, any>[] // Check if `Store` is `object[]`
     ? // Connect path. e.g. { a: { b: string }[] }
       // Get: [a] | [ a,number] | [ a ,number , b]
-      | [...ParentNamePath, number]
-        | DeepNamePath<PureObject<Store[number]>, [...ParentNamePath, number]>
+      | []
+        | [...ParentNamePath, number]
+        | DeepNamePathBase<Store[number], [...ParentNamePath, number]>
     : Store extends Record<string, any> // Check if `Store` is `object`
     ? {
         // Convert `Store` to <key, value>. We mark key a `FieldKey`
@@ -32,13 +25,15 @@ export type DeepNamePath<Store = any, ParentNamePath extends any[] = []> =
           | (Store[FieldKey] extends (number | string)[]
               ? [...ParentNamePath, FieldKey, number] // If `Store[FieldKey]` is base array type
               : Store[FieldKey] extends Record<string, any>
-              ? DeepNamePath<PureObject<Store[FieldKey]>, [...ParentNamePath, FieldKey]> // If `Store[FieldKey]` is object
+              ? DeepNamePathBase<Store[FieldKey], [...ParentNamePath, FieldKey]> // If `Store[FieldKey]` is object
               : never);
       }[keyof Store]
     : never;
 
-type Test = {
-  date: Moment;
+type IsOptional<Key extends keyof Obj, Obj> = {} extends Pick<Obj, Key> ? Key : never;
+
+export type DeepRequired<T = any> = {
+  [Key in keyof T]-?: IsOptional<Key, T> extends never ? T[Key] : DeepRequired<T[Key]>;
 };
 
-type CCC = DeepNamePath<Test>;
+export type DeepNamePath<T = any> = DeepNamePathBase<DeepRequired<T>>;
