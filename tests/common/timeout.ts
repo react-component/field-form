9ee94c4fd5f6f2348bcfa050e0eabe1eaed36695
二货机器人 <smith3816@gmail.com>
2023-10-17 17:13:05 +0800

    feat: validate support dirty

--- tests/common/timeout.ts
+++ tests/common/timeout.ts
@@ -1,5 +1,15 @@
+import { act } from '@testing-library/react';
+
 export default async (timeout: number = 10) => {
   return new Promise<void>(resolve => {
     setTimeout(resolve, timeout);
   });
 };
+
+export async function waitFakeTime(timeout: number = 10) {
+  await act(async () => {
+    await Promise.resolve();
+    jest.advanceTimersByTime(timeout);
+    await Promise.resolve();
+  });
+}
