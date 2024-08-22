674cc8f5b0012c7b6a72cafd669b477abedbd783
clydee <641365724@qq.com>
2024-08-22 18:55:17 +0800

    fix: 修复”RangePicker组件在onCalendarChange方法中调用form.setFieldsValue会导致时间不可选择“的问题

--- src/Field.tsx
+++ src/Field.tsx
@@ -46,7 +46,8 @@ function requireUpdate(
   if (typeof shouldUpdate === 'function') {
     return shouldUpdate(prev, next, 'source' in info ? { source: info.source } : {});
   }
-  return prevValue !== nextValue;
+  // return prevValue !== nextValue;
+  return !isEqual(prev, next)
 }
 
 // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
