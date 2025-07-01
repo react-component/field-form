27328cb910d01e15bd526d2a22aff8c1d2b9c407
二货机器人 <smith3816@gmail.com>
2025-07-01 16:03:28 +0800

    feat: useWatch support dynamic names

--- docs/examples/debug.tsx
+++ docs/examples/debug.tsx
@@ -1,51 +1,30 @@
 import Form, { Field } from 'rc-field-form';
 import React from 'react';
-
-function WatchCom({ name }: { name: number }) {
-  const data = Form.useWatch(name);
-  return data;
-}
+import Input from './components/Input';
 
 export default function App() {
   const [form] = Form.useForm();
-  const [open, setOpen] = React.useState<boolean>(true);
+  const [keyName, setKeyName] = React.useState(true);
 
-  const data = React.useMemo(() => {
-    return Array.from({ length: 1 * 500 }).map((_, i) => ({
-      key: i,
-      name: `Edward King ${i}`,
-      age: 32,
-      address: `London, Park Lane no. ${i}`,
-    }));
-  }, []);
+  // const val = Form.useWatch(keyName ? 'name' : 'age', form);
+  const val = Form.useWatch(values => values[keyName ? 'name' : 'age'], form);
 
   return (
     <Form form={form}>
-      <div className="App">
-        {/* When I made the switch, it was very laggy */}
-        <button
-          onClick={() => {
-            setOpen(!open);
-          }}
-        >
-          Switch
-        </button>
-        <WatchCom name={0} />
-        <WatchCom name={1} />
-        {open ? (
-          <div className="flex gap-[5px] flex-wrap">
-            {data?.map(item => {
-              return (
-                <Field key={item.name} name={item.name}>
-                  <input />
-                </Field>
-              );
-            })}
-          </div>
-        ) : (
-          <h2>some thing</h2>
-        )}
-      </div>
+      <button
+        onClick={() => {
+          setKeyName(!keyName);
+        }}
+      >
+        Switch {String(keyName)}
+      </button>
+      <Field name="name" initialValue="bamboo">
+        <Input />
+      </Field>
+      <Field name="age" initialValue="light">
+        <Input />
+      </Field>
+      {val}
     </Form>
   );
 }
