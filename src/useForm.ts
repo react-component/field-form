e226ffea8774304eecaa4d0121fd52157f6bcf93
二货机器人 <smith3816@gmail.com>
2025-12-08 17:30:29 +0800

    chore: update values

--- src/useForm.ts
+++ src/useForm.ts
@@ -1,4 +1,5 @@
 import { merge } from '@rc-component/util/lib/utils/set';
+import { mergeWith } from '@rc-component/util';
 import warning from '@rc-component/util/lib/warning';
 import * as React from 'react';
 import { HOOK_MARK } from './FieldContext';
@@ -786,7 +787,10 @@ export class FormStore {
       const changedValues = cloneByNamePathList(this.store, [namePath]);
       const allValues = this.getFieldsValue();
       // Merge changedValues into allValues to ensure allValues contains the latest changes
-      const mergedAllValues = merge(allValues, changedValues);
+      const mergedAllValues = mergeWith([allValues, changedValues], {
+        // When value is array, it means trigger by Form.List which should replace directly
+        prepareArray: current => (Array.isArray(value) ? [] : [...(current || [])]),
+      });
       onValuesChange(changedValues, mergedAllValues);
     }
 
