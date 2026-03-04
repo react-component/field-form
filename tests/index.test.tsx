7a40329355f3c29a7430935f0d4f570f4da89719
二货机器人 <smith3816@gmail.com>
2026-03-04 15:14:18 +0800

    fix: restore missing type imports in tests

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -7,6 +7,7 @@ import { changeValue, getInput, matchError } from './common';
 import InfoField, { Input } from './common/InfoField';
 import timeout, { waitFakeTime } from './common/timeout';
 
+import type { FormRef, Meta } from '@/interface';
 describe('Form.Basic', () => {
   describe('create form', () => {
     const Content: React.FC = () => (
