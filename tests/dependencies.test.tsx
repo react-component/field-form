5f0189519ba5ff0fe0f38dc6d617bf5e745bd592
二货机器人 <smith3816@gmail.com>
2026-03-23 11:59:20 +0800

    test: cover nested list remove onValuesChange

--- tests/dependencies.test.tsx
+++ tests/dependencies.test.tsx
@@ -347,4 +347,49 @@ describe('Form.Dependencies', () => {
       { rules: [{ name: 'test', triggers: ['trigger_1'] }] },
     );
   });
+
+  it('mixed field list remove should not missing value with onValuesChange', () => {
+    const onValuesChange = jest.fn();
+
+    const { container } = render(
+      <Form
+        initialValues={{ rules: [{ name: 'test', triggers: ['trigger_1', 'trigger_2'] }] }}
+        onValuesChange={onValuesChange}
+      >
+        <Form.List name="rules">
+          {fields =>
+            fields.map(field => (
+              <div key={field.key}>
+                <Field {...field} name={[field.name, 'name']}>
+                  <Input />
+                </Field>
+
+                <Form.List name={[field.name, 'triggers']}>
+                  {(triggerFields, { remove }) => (
+                    <>
+                      {triggerFields.map(triggerField => (
+                        <Field {...triggerField} key={triggerField.key}>
+                          <Input />
+                        </Field>
+                      ))}
+                      <button type="button" onClick={() => remove(0)}>
+                        Remove trigger
+                      </button>
+                    </>
+                  )}
+                </Form.List>
+              </div>
+            ))
+          }
+        </Form.List>
+      </Form>,
+    );
+
+    fireEvent.click(container.querySelector('button')!);
+
+    expect(onValuesChange).toHaveBeenLastCalledWith(
+      expect.anything(),
+      { rules: [{ name: 'test', triggers: ['trigger_2'] }] },
+    );
+  });
 });
